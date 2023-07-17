2f69953464365a20b48b45f203adda7d17bc0f31
unknown <fabricio.rivera2012@gmail.com>
2023-07-16 23:10:11 -0300

    ajustando el tamaño de las imagenes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,12 +4,12 @@ import { img } from '../data';
 
 export const MainPage = () => {
   return (
-    <div className="absolute top-0 left-[18rem] w-[calc(100%-18rem)] h-[200vh] bg-slate-700 overflow-hidden">
+    <div className="absolute top-5 left-[18rem] w-[calc(100%-18rem)] h-[200vh] overflow-hidden">
         <div>
-          <div className='flex flex-wrap'>
+          <div className='flex flex-wrap gap-10'>
             {img.map((elem) => {
               return (
-                <div key={elem.id} className='w-2/5'>
+                <div key={elem.id} className='w-[calc((100%-18rem)/1.7)]'>
                   <img src={`./src/img/${elem.name}.jpg`} alt={elem.title} className='w-full'/>
                   {/* <div className="p-4">
                     <h2 className="text-xl font-bold">{elem.title}</h2>
