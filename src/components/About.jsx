717120cbd0fa1fb36e78261ffa7aa96a48fc2671
unknown <fabricio.rivera2012@gmail.com>
2023-07-20 08:26:18 -0300

    changing the about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,24 +3,43 @@
 
 export const About = () => {
   return (
-    <div className="absolute top-5 p-10 left-[16rem] w-[calc(100%-17rem)] h-[calc(100vh-20px)] overflow-hidden">
+    <div className="absolute top-5 p-10 left-[16rem] w-[calc(100%-17rem)] h-[calc(100vh-20px)]">
         <div>
-            <h2 className="box-decoration-slice bg-gradient-to-r from-amber-200 to-slate-200 text-black px-2 py-2">¿Quien soy?</h2>
-            <h4 className="box-decoration-clone bg-gradient-to-r from-amber-200 to-slate-200 text-black px-4">Me llamo Fabricio Rivera.</h4>
-            <p className="p-4">Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Dolores earum suscipit et voluptatum in alias 
-            optio, repudiandae autem illo, omnis, laboriosam veritatis iste at 
-            veniam consectetur temporibus distinctio provident cum recusandae 
-            perspiciatis! Accusamus rerum distinctio natus fugiat, doloribus 
-            odio quis provident impedit eius at nihil aspernatur id laborum 
-            voluptatum illum quas ipsam earum ad? Repellendus laboriosam culpa, 
-            optio, suscipit cumque voluptates nisi adipisci veritatis aut 
-            dolorum quisquam nam, dolorem ipsam quia voluptatibus corrupti 
-            maiores sit. Assumenda, provident tempora aliquid ratione, sunt 
-            vitae dolores sequi similique delectus in voluptate aperiam nemo 
-            tempore doloribus laudantium veniam? Quam blanditiis dignissimos 
-            totam quia asperiores!
-            </p>
+            <h2 className="px-4">Hola!</h2>
+            <hr />
+            <h5 className="px-4 py-2">Bienvenido a mi pagina dedicada a la fotografia</h5>
+            {/* <h4 className="box-decoration-clone bg-gradient-to-r from-amber-200 to-slate-200 text-black px-4">a </h4> */}
+            <div className="columns-3">
+                <p className="p-4">Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Dolores earum suscipit et voluptatum in alias 
+                optio, repudiandae autem illo, omnis, laboriosam veritatis iste at 
+                veniam consectetur temporibus distinctio provident cum recusandae 
+                perspiciatis! Accusamus rerum distinctio natus fugiat, doloribus 
+                odio quis provident impedit eius at nihil aspernatur id laborum 
+                voluptatum illum quas ipsam earum ad? Repellendus laboriosam culpa, 
+                optio, suscipit cumque voluptates nisi adipisci veritatis aut 
+                dolorum quisquam nam, dolorem ipsam quia voluptatibus corrupti 
+                maiores sit. Assumenda, provident tempora aliquid ratione, sunt 
+                vitae dolores sequi similique delectus in voluptate aperiam nemo 
+                tempore doloribus laudantium veniam? Quam blanditiis dignissimos 
+                totam quia asperiores!
+                </p>
+                <img src="./src/img/img (2).jpg" alt="img" />
+                <p className="p-4">Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Dolores earum suscipit et voluptatum in alias 
+                optio, repudiandae autem illo, omnis, laboriosam veritatis iste at 
+                veniam consectetur temporibus distinctio provident cum recusandae 
+                perspiciatis! Accusamus rerum distinctio natus fugiat, doloribus 
+                odio quis provident impedit eius at nihil aspernatur id laborum 
+                voluptatum illum quas ipsam earum ad? Repellendus laboriosam culpa, 
+                optio, suscipit cumque voluptates nisi adipisci veritatis aut 
+                dolorum quisquam nam, dolorem ipsam quia voluptatibus corrupti 
+                maiores sit. Assumenda, provident tempora aliquid ratione, sunt 
+                vitae dolores sequi similique delectus in voluptate aperiam nemo 
+                tempore doloribus laudantium veniam? Quam blanditiis dignissimos 
+                totam quia asperiores!
+                </p>
+            </div>
         </div>
     </div>
   )
