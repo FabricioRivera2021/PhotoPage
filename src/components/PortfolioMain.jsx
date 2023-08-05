66b88fa7cdc643c516b16e2b0a951a86d0545c99
unknown <fabricio.rivera2012@gmail.com>
2023-08-05 00:26:03 -0300

    trabajando en la pagina de portfolio main

--- src/components/PortfolioMain.jsx
+++ src/components/PortfolioMain.jsx
@@ -2,25 +2,26 @@ import {NavLink} from 'react-router-dom'
 
 export const PortfolioMain = () => {
   return (
-    <div className="absolute top-0 left-[16rem] w-[calc(100%-16rem)] md:px-6 h-[100vh]">
-      <div className="absolute top-0 w-full left-0 h-full bg-opacity-60">
+    <div className="relative top-0 left-[16rem] w-[calc(100%-16rem)] h-[100vh]">
+      <div className="w-full h-full bg-opacity-60">
 
-        <div className="relative flex justify-center space-x-20">
-          <NavLink>
-            <div className="flex-1 relative top-20 w-[350px] bg-cover m-auto bg-sesionesBg-img h-[700px] hover:h-[720px] hover:bg-center transition-all">
-              <h3 className='uppercase text-center bg-slate-600 text-white p-4 bg-opacity-70'>
-                sesiones
-              </h3>  
-            </div>  
-          </NavLink>    
-          <NavLink>
+        <div className="flex w-full h-full justify-around gap-5 p-6 items-center">
+          <NavLink className="flex-1">
             <div className="card-zoom">
               <div className='card-zoom-image bg-streetBg-img'></div>
+              <h3 className='card-zoom-text'>Sesiones de estudio</h3>
+            </div>
+          </NavLink>    
+          <NavLink className="flex-1">
+            <div className="card-zoom">
+              <div className='card-zoom-image bg-streetBg-img'></div>
+              <h3 className='card-zoom-text'>Eventos</h3>
             </div>
           </NavLink>
-          <NavLink>
-            <div className="flex-1 relative top-28 w-[350px] bg-center bg-cover m-auto bg-streetBg-img h-[700px]">
-              <h3 className='uppercase text-center bg-slate-600 text-white p-4 bg-opacity-70'>fotografia callejera</h3>  
+          <NavLink className="flex-1">
+            <div className="card-zoom">
+              <div className='card-zoom-image bg-streetBg-img'></div>
+              <h3 className='card-zoom-text'>Fotógrafia callejera</h3>
             </div>
           </NavLink>
         </div>
