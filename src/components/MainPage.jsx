9ecc0fbd3ef7d5aa1cb929d071975627c196b7c0
unknown <fabricio.rivera2012@gmail.com>
2023-08-05 23:07:20 -0300

    mas fixes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,25 +1,10 @@
-import { img } from '../data';
-
 
 
 export const MainPage = () => {
   return (
     <div className="absolute top-5 left-[16rem] w-[calc(100%-17rem)] overflow-hidden">
-        <div>
-          <div className='columns-1 md:columns-2 xl:columns-3 gap-2 mb-10'>
-            {img.map((elem) => {
-              return (
-                <div key={elem.id} className='mb-2 hover:cursor-pointer hover:opacity-70'>
-                  <img src={`./src/img/${elem.name}.jpg`} alt={elem.title}/>
-                  {/* <div className="p-4">
-                    <h2 className="text-xl font-bold">{elem.title}</h2>
-                    <p>{elem.desc}</p>
-                  </div> */}
-                </div>
-              );
-            })}
-            </div>
-          </div>
+      <h1>Bienvenido a fabricio rivera fotografia</h1>
+      <p>Esto es una pagina de prueba</p>
     </div>
   )
 }
