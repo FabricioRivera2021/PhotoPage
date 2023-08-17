dc35b7fe242a1a52e95ffe786a86bb5b688efe30
unknown <fabricio.rivera2012@gmail.com>
2023-08-17 09:44:26 -0300

    puliendo el slide y añadiendo botones para cambiar imagen

--- src/components/MainSlides.jsx
+++ src/components/MainSlides.jsx
@@ -7,22 +7,30 @@ export const MainSlides = ({slides}) => {
 
     const [currentImg, setCurrentImg] = useState(0);
 
+    let timer = setTimeout(() => {
+        if(currentImg >= 500){
+            setCurrentImg(0)
+        }else{
+            setCurrentImg(currentImg + 100)
+        }
+    }, 5000);
+
     useEffect(() => {
-        setTimeout(() => {
-            if(currentImg >= 500){
-                setCurrentImg(0)
-            }else{
-                setCurrentImg(currentImg + 100)
-            }
-        }, 5000);
+        timer
+
+        return () => {
+            clearTimeout(timer)
+        }
+
     }, [currentImg])
 
     return (
     <div className="absolute left-[16rem] h-screen w-[calc(100%-16rem)] flex flex-col justify-center items-center bg-gradient-to-r from-zinc-900 to-transparent animate-gradient-x">
-        <div className="antialiased text-center pb-3 drop-shadow-lg bg-gradient-to-r from-stone-400 via-slate-200 to-zinc-50 text-transparent bg-clip-text">
-            <h1 className="font-normal uppercase p-2 text-center">Fabricio Rivera <span className="font-black tracking-[0.5rem]"><br />Fotográfia</span></h1>
-            <p className="text-xl">Hola y bienvenido a mi página dedicada a la fotográfia, me especializo en sesiones tanto en exteriores como en estudio.</p>
-            <p>Tambien hago fotografia de producto, te invito a que veas mis trabajos en la sección de portafolio.</p>
+        <div className="antialiased text-center pb-3 drop-shadow-xl bg-gradient-to-r from-stone-400 via-slate-200 to-zinc-50 text-transparent bg-clip-text">
+            <h1 className="font-normal tracking-widest uppercase p-2 text-zinc-800">Fabricio Rivera <span className="font-black tracking-[0.5rem] text-zinc-800"><br />Fotografía</span></h1>
+            <hr />
+            <p className="text-xl">Hola y bienvenido a mi página dedicada a la fotografía, me especializo en realizar sesiones de retrato tanto en exteriores como en estudio.</p>
+            <p>Tambien hago fotografía de producto, te invito a que veas mis trabajos en la sección de portafolio.</p>
         </div>
         <div className="mb-4 h-[75vh] w-[calc(100%-38rem)] overflow-hidden">
             <div className='h-full mx-auto bg-contain bg-no-repeat bg-center transition-transform ease-out duration-1000 flex items-center' 
@@ -39,9 +47,12 @@ export const MainSlides = ({slides}) => {
             {slides.map((elem, index) => (
                 (currentImg === (index * 100) )
                     ?
-                    <div key={index} className="w-5 h-[2px] bg-white"></div>
+                    <div key={index} className="w-5 h-[5px] bg-white hover:cursor-pointer rounded-lg"></div>
                     :
-                    <div key={index} className="w-5 h-[2px] bg-slate-900"></div>
+                    <div key={index} className="w-5 h-[5px] bg-zinc-700 hover:cursor-pointer rounded-lg" 
+                         onClick={()=>{
+                            setCurrentImg(index * 100)
+                         }}></div>
             ))}
         </div>
     </div>
--- src/mainPageSlides.js
+++ src/mainPageSlides.js
@@ -3,7 +3,7 @@ export const slides = [
       name: "./src/img/exteriores (2).jpg"
     },
     {
-      name: "./src/img/mainPageImg (0).jpg"
+      name: "./src/img/mainPageImg (5).png"
     },
     {
       name: "./src/img/mainPageImg (4).png"
