0c8caf15b0abc4dc6f1a82a65eb2634389ac8e6e
unknown <fabricio.rivera2012@gmail.com>
2023-09-05 21:10:16 -0300

    haciendo el mobile del about y el desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,41 +3,25 @@
 
 export const About = () => {
   return (
-    <div className="absolute top-5 p-10 left-[16rem] w-[calc(100%-17rem)] h-[calc(100vh-20px)]">
+    <div className="absolute sm:top-5 top-20 sm:p-10 p-5 sm:left-[16rem] sm:w-[calc(100%-17rem)] h-[calc(100vh-20px)]">
         <div>
-            <h2 className="px-4">Hola!</h2>
+            <h2 className="sm:px-0 px-4">Hola !</h2>
             <hr />
-            <h5 className="px-4 py-2">Bienvenido a mi pagina dedicada a la fotografia</h5>
+            <h5 className="py-3 text-3xl">Bienvenido a mi pagina dedicada a la fotografia</h5>
             {/* <h4 className="box-decoration-clone bg-gradient-to-r from-amber-200 to-slate-200 text-black px-4">a </h4> */}
-            <div className="columns-3">
-                <p className="p-4">Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Dolores earum suscipit et voluptatum in alias 
-                optio, repudiandae autem illo, omnis, laboriosam veritatis iste at 
-                veniam consectetur temporibus distinctio provident cum recusandae 
-                perspiciatis! Accusamus rerum distinctio natus fugiat, doloribus 
-                odio quis provident impedit eius at nihil aspernatur id laborum 
-                voluptatum illum quas ipsam earum ad? Repellendus laboriosam culpa, 
-                optio, suscipit cumque voluptates nisi adipisci veritatis aut 
-                dolorum quisquam nam, dolorem ipsam quia voluptatibus corrupti 
-                maiores sit. Assumenda, provident tempora aliquid ratione, sunt 
-                vitae dolores sequi similique delectus in voluptate aperiam nemo 
-                tempore doloribus laudantium veniam? Quam blanditiis dignissimos 
-                totam quia asperiores!
+            <div>
+                <p className="text-lg pb-5">Me llamo Fabricio Rivera y me dedico principalmente a la fotografia en estudio, ya sea para retratos o productos. Ademas tambien hago sesiones en exteriores,
+                siempre trato de transmitir mi pasion por la fotos en todas las sesiones que realizo. 
                 </p>
-                <p className="p-4">Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Dolores earum suscipit et voluptatum in alias 
-                optio, repudiandae autem illo, omnis, laboriosam veritatis iste at 
-                veniam consectetur temporibus distinctio provident cum recusandae 
-                perspiciatis! Accusamus rerum distinctio natus fugiat, doloribus 
-                odio quis provident impedit eius at nihil aspernatur id laborum 
-                voluptatum illum quas ipsam earum ad? Repellendus laboriosam culpa, 
-                optio, suscipit cumque voluptates nisi adipisci veritatis aut 
-                dolorum quisquam nam, dolorem ipsam quia voluptatibus corrupti 
-                maiores sit. Assumenda, provident tempora aliquid ratione, sunt 
-                vitae dolores sequi similique delectus in voluptate aperiam nemo 
-                tempore doloribus laudantium veniam? Quam blanditiis dignissimos 
-                totam quia asperiores!
-                </p>
+                <div className="columns-2">
+                  <img className="px-3 sm:w-[50rem]" src=".\src\img\aboutimg.jpg" alt="image" />
+                  <p className="text-lg px-4 sm:px-0 pt-2">Me encanta hacer retratos, siempre tratando de sacar la mejor imagen posible de la persona retratada. Cada persona es diferente y es mi trabajo buscar y conseguir 
+                  una imagen interesante y profesional.
+                  </p>
+                  <p className="text-lg py-4">Si estas interesado en agendarte para una sesion de fotos, o queres hacer una sesion para algun producto que estes promocionando puedes contactarte conmigo desde la pestaña de contacto, 
+                  para mi sera un placer ayudarte.
+                  </p>
+                </div>
             </div>
         </div>
     </div>
